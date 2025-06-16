f918cc3e384c8261436cd67287397705c20f1d00
Кашин Савва Игоревич <s.kashin@els24.com>
2025-06-16 22:38:24 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import MainNavigation from '@/components/MainNavigation.vue'
+</script>
+
+<template>
+  <div>
+    <MainNavigation />
+
+    <router-view></router-view>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/views/GameView.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+import Clicker from '@/components/clicker/Clicker.vue'
+</script>
+
+<template>
+<div>
+  <Clicker></Clicker>
+</div>
+</template>
+
+<style scoped>
+
+</style>
